70a7580696970eb23e27c314fefff53e745ef1b8
HZWYL <952942236@qq.com>
2016-07-21 16:43:09 +0800

    Initial Commit

---
+++ saveImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  saveImage
+//
+//  Created by ycd15 on 16/7/21.
+//  Copyright © 2016年 YCD_WYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
